6367d66bfed57803347515f99b1b2a3e9352721e
stephen911 <stephendappah1@gmail.com>
2022-03-20 16:34:13 +0000

    'new'

--- student.py
+++ student.py
@@ -45,4 +45,5 @@ def manageStudent(): ~Function For The Student Management System
 																																			   		if(newStd in listStd): ~This Condition Checking The New Student Is Already In List Ur Not
 																																					   			print("\nThis Student {} Already In The Database".format(newStd))  ~Error Message
 																																								   		else:	
-																																											   )
+																																											   			listStd.append(newStd)
+																																														   )
