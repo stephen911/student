cece86836958b1c76053552157438555788d69f8
stephen911 <stephendappah1@gmail.com>
2022-03-20 16:29:32 +0000

    'new'

--- student.py
+++ student.py
@@ -11,4 +11,5 @@ def manageStudent(): ~Function For The Student Management System
 			global bye ~Making Bye As Super Global Variable
 				bye = "\n {}\n~ {} ~\n~ ===> Brought To You By <===  ~\n~ ===> code-projects.org <===  ~\n~ {} ~\n {}".format(x, y, y, x) ~ Will Print GoodBye Message
 
-				
+					~Printing Welcome Message And options For This Program
+					
