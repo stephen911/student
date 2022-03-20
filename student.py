fc35fc59074c772e5d4f6585ac263b54658ccee5
stephen911 <stephendappah1@gmail.com>
2022-03-20 16:28:35 +0000

    'new'

--- student.py
+++ student.py
@@ -4,3 +4,4 @@ import platform
 global listStd ~Making ListStd As Super Global Variable
 listStd = ["yugesh", "kishor", "gajen", "Gopi"] ~List Of Students
 
+def manageStudent(): ~Function For The Student Management System
